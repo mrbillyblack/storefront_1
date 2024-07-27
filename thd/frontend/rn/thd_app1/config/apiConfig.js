15d26126e881f9adea7b89bf45d11407928640d5
Billy Black <wmcguire1@hotmail.com>
2024-07-26 22:12:25 -0400

    corrected configuration

--- thd/frontend/rn/thd_app1/config/apiConfig.js
+++ thd/frontend/rn/thd_app1/config/apiConfig.js
@@ -2,7 +2,7 @@
 import { createGlobalState } from 'react-hooks-global-state';
 
 
-const API_URL = 'http://192.168.1.230/api';  // Replace with your actual FastAPI backend URL
+const API_URL = 'https://yourserver.com/api';  // Replace with your actual FastAPI backend URL
 
 
 const {setGlobalState, useGlobalState} = createGlobalState({
